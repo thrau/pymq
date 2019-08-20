04ed3c06b4c95cbcfa8f37a0fc5306f4451bdd12
Thomas Rausch <thomas@rauschig.org>
2019-08-20 00:58:29 -0400

    Prepare release v0.1.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("requirements.txt", "r") as fh:
 
 setuptools.setup(
     name="pymq",
-    version="0.2.0.dev1",
+    version="0.1.1",
     author="Thomas Rausch",
     author_email="thomas@rauschig.org",
     description="A simple message-oriented middleware library built for Python IPC across machine boundaries",
