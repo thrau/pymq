16558da1da2c6d1ff1813c100f466b44f8456257
Thomas Rausch <thomas@thrau.at>
2022-04-26 23:29:26 +0200

    add experimental AWS provider (#12)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,44 @@
+import logging
 import os
 import shutil
 import sys
 
 import pytest
+from localstack.utils.bootstrap import LocalstackContainer, LocalstackContainerServer
 
 import pymq as pymq
 
+LOG = logging.getLogger(__name__)
 
-@pytest.fixture(params=["init_simple", "init_redis", "init_ipc"])
+
+def pytest_runtest_setup(item):
+    # handle xfail_provider marker
+
+    try:
+        init_fn = item.callspec.params.get("pymq_init")
+    except AttributeError:
+        # functions without arguments don't have callspecs
+        return
+
+    if not init_fn:
+        return
+
+    for marker in item.iter_markers():
+        if marker.name == "xfail_provider":
+            if init_fn in marker.args:
+                pytest.xfail("text expected to fail for %s" % init_fn)
+
+
+@pytest.fixture(params=["init_simple", "init_redis", "init_ipc", "init_aws"])
 def pymq_init(request):
     """parameterized pymq_init fixture (a fixture that, when called, initializes pymq)"""
 
     if request.param == "init_ipc" and sys.platform != "linux":
         pytest.skip("IPC provider only works for linux")
+        return None
+
+    if request.param == "init_aws" and not shutil.which("docker"):
+        pytest.skip("Cannot test AWS provider without docker (needed to run localstack)")
         return None
 
     return request.getfixturevalue(request.param)
@@ -166,3 +192,51 @@ def init_redis(redislite):
 def pymq_redis(init_redis):
     init_redis()
     yield pymq
+
+
+# aws provider through localstack
+
+
+@pytest.fixture()
+def pymq_aws(init_aws):
+    init_aws()
+    yield pymq
+
+
+@pytest.fixture()
+def init_aws(localstack):
+    from pymq.provider.aws import AwsEventBus, LocalstackConfig
+
+    config = LocalstackConfig(endpoint_url=localstack.url)
+
+    _bus_container = []
+    _invalid = False
+
+    def _init() -> AwsEventBus:
+        if _bus_container:
+            raise ValueError("already called")
+        if _invalid:
+            raise ValueError("expired init function")
+
+        bus = pymq.init(config)
+        _bus_container.append(bus)
+        return bus
+
+    yield _init
+
+    _invalid = True
+
+    if _bus_container:
+        pymq.shutdown()
+        _bus_container[0].close()
+
+
+@pytest.fixture(scope="class")
+def localstack():
+    container = LocalstackContainer()
+    container.ports.add(4566, 4566)
+    ls = LocalstackContainerServer(container)
+    ls.start()
+    ls.wait_is_up()
+    yield ls
+    ls.shutdown()
